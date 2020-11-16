41c733615f513399f57948c4aba96bf1bd569445
propellasallad <propellasallad@users.noreply.github.com>
2020-11-16 14:48:11 +1300

    Initial commit

---
+++ CryptoApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using CryptoApp.Model;
+
+namespace CryptoApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private string apiKey = "91B040B7-5087-4873-AC12-3E5151FE6B10";
+        private string baseImageUrl = "https://s3.eu-central-1.amazonaws.com/bbxt-static-icons/type-id/png_64/";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            coinListView.ItemsSource = GetCoins();
+        }
+
+        private void RefreshButton_Clicked(object sender, EventArgs e)
+        {
+            coinListView.ItemsSource = GetCoins();
+        }
+
+        private List<Coin> GetCoins()
+        {
+            List<Coin> coins;
+
+            var client = new RestClient("https://rest.coinapi.io/v1/assets?filter_asset_id=BTC;ETH;LTC;XMR;XRP;DASH");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("X-CoinAPI-Key", apiKey);
+
+            var response = client.Execute(request);
+
+            coins = JsonConvert.DeserializeObject<List<Coin>>(response.Content);
+
+            foreach (var c in coins)
+            {
+                if (!String.IsNullOrEmpty(c.Id_Icon))
+                    c.Icon_Url = baseImageUrl + c.Id_Icon.Replace("-", "") + ".png";
+                else
+                    c.Icon_Url = "";
+            }
+
+            return coins;
+        }
+    }
+}
---
+++ CryptoApp/Model/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoApp.Model
+{
+    public class Coin
+    {
+        public string Asset_Id { get; set; }
+        public string Name { get; set; }
+        public float Price_usd { get; set; }
+        public string Id_Icon { get; set; }
+        public string Icon_Url { get; set; }
+    }
+}
